87171f6f8184afcf1317ea26de4e0c4774526708
0xH4sh <keivan996a@gmail.com>
2025-12-09 07:34:22 +0330

    Update Number_of_letters.py

--- Number_of_letters.py
+++ Number_of_letters.py
@@ -1,4 +1,4 @@
-my_Dict = {}
+my_Dict = {}    # dictionary for saving the number of letters
 while True:
     user_text = input('please enter a text or sentence (To exit, press "exit"): ')  # user text or sentence
 
@@ -18,15 +18,15 @@ while True:
 
     j = 0
     while j < len(user_text):
-        char = user_text[j]
+        char = user_text[j]     # "j" assigned to "char"
         if char in my_Dict:
-            my_Dict[char] += 1
+            my_Dict[char] += 1  # Add if available
         else:
-            my_Dict[char] = 1
+            my_Dict[char] = 1   # Add if not present
         j += 1
 
 
-if not my_Dict:
+if not my_Dict:     # check for non-empty
     print('Nothing Added!')
 else:
     print(my_Dict)
